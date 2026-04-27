2bac70f61164299cd800c74a85ccb6abe26a5aaf
Isamara Costa <jcisamara@gmail.com>
2026-04-27 16:09:47 +0000

    estudo27/04/

---
+++ ExercicioP41/LetraA.java
@@ -0,0 +1,14 @@
+//4-a) Ler dois valores numéricos inteiros e apresentar 
+// o resultado da diferença do maior pelo menor valor.
+
+void main(){
+
+    float A = Float.parseFloat(IO.readln("Informe o primeiro Numero: "));
+    float B = Float.parseFloat(IO.readln("Informe o segundo Numero: "));
+
+    if(A>B)
+        IO.println("A diferença dos número é " , (A-B));
+    else (B > A)    
+        IO.println("A diferença dos número é " , (B-A));
+
+}
